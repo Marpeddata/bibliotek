641e84643faa8c3b8a0f5b809906b3ba1dba94d4
Mark Pedersen <sutsko@gmail.com>
2022-09-19 14:08:21 +0200

    test function

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,6 +21,7 @@ public class Main {
 
                 case "mark":
                     System.out.println(DBConnector.getMarkConnection());
+                    printNames();
                     break;
                 case "malene":
                     System.out.println(DBConnector.getMaleneConnection());
@@ -22,6 +30,35 @@ public class Main {
         }
 
 
+    }
+
+    static void printNames() {
+        List<String> kundeList = new ArrayList<>();
+
+        String sql = "select * from lånertabel";
+
+        try (Connection con = DBConnector.getMarkConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {           // https://en.wikipedia.org/wiki/Prepared_statement
+
+
+            ResultSet resultSet = ps.executeQuery();   //https://javaconceptoftheday.com/difference-between-executequery-executeupdate-execute-in-jdbc/
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("postnr");
+                String navn = resultSet.getString("navn");
+
+                kundeList.add(id + " : " + navn);
+            }
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+
+        for (String s : kundeList) {
+
+            System.out.println(s);
+
+        }
     }
 
 
