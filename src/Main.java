411f7fccce34892eafce4d26f3de7801b34e8051
Mark Pedersen <sutsko@gmail.com>
2022-09-19 15:08:10 +0200

    book creation

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         Udlån lån = new Udlån();
 
-        lån.opretLåner();
+        lån.opretBog();
 
 
 //        String svar;
--- src/Udlån.java
+++ src/Udlån.java
@@ -12,7 +12,7 @@ public class Udlån {
         int postNr;
 
 
-        lånerNavn = terminal.getString("Skriv navn:");
+        lånerNavn = terminal.getString("Skriv navn");
         Adresse = terminal.getString("Skriv adresse");
         postNr = terminal.getInt("Skriv postnummer");
 
@@ -30,6 +30,47 @@ public class Udlån {
             ps.setString(1, lånerNavn);
             ps.setString(2, Adresse);
             ps.setInt(3, postNr);
+
+
+            ps.executeUpdate();    //https://javaconceptoftheday.com/difference-between-executequery-executeupdate-execute-in-jdbc/
+
+//            ResultSet ids = ps.getGeneratedKeys();
+//            ids.next();
+//            int id = ids.getInt(1);
+//
+//
+//            System.out.println("Vi er nået til række : " + id);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void opretBog(){
+        String forfatterNavn;
+        String titel;
+        int udgivelsesÅr;
+
+
+        forfatterNavn = terminal.getString("Skriv forfatterens navn");
+        titel = terminal.getString("Skriv bogens titel");
+        udgivelsesÅr = terminal.getInt("Skriv udgivelsesår");
+
+        String sql = "INSERT INTO bogtabel (forfatternavn, titel, udgivelsesår) VALUES (?,?,?)";
+
+
+        // se lige try-with-resources f.eks. her  https://www.baeldung.com/java-try-with-resources
+        try (Connection con = DBConnector.getMarkConnection();  // får en connection
+
+             // se evt. https://docs.oracle.com/javase/tutorial/jdbc/basics/prepared.html
+             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        ) {
+
+            // her klargøres mit prepared statement ved at min parametre indsættes.
+            ps.setString(1, forfatterNavn);
+            ps.setString(2, titel);
+            ps.setInt(3, udgivelsesÅr);
 
 
             ps.executeUpdate();    //https://javaconceptoftheday.com/difference-between-executequery-executeupdate-execute-in-jdbc/
