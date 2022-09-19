af1902f94bf7c17bc5faf8f1aa588abc6a422e67
Mark Pedersen <sutsko@gmail.com>
2022-09-19 13:48:15 +0200

    creating input class

---
+++ src/TerminalIO.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class TerminalIO {
+
+    public static String getString(String s) {
+
+        System.out.println(s + ": ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+
+    }
+
+    public static int getInt(String s){
+
+        while (true) {
+            try {
+                int ans = Integer.parseInt(getString(s));
+                return ans;
+            } catch (NumberFormatException e) {
+                System.out.println("husk ikke et tal ord");
+            }
+        }
+    }
+
+}
